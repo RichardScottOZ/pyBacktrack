6655ac7638c94b889e8b2d21d6c2a492b09e8d0c
John Cannon <john.cannon@sydney.edu.au>
2025-08-12 21:47:08 +1000

    Improve trench association between two similar reconstruction models.
    Only need uniform points in 'dst' trench (not 'src' trench).

--- pybacktrack/supplementary/transfer_trench_exclusion_distances.py
+++ pybacktrack/supplementary/transfer_trench_exclusion_distances.py
@@ -30,23 +30,12 @@ for dst_trench_feature in dst_trench_features:
     # Go through all src trenches to find the closest one to the current dst trench.
     for src_trench_feature in src_trench_features:
         src_trench_line = src_trench_feature.get_geometry()
-        src_trench_points = src_trench_line.to_uniform_points(uniform_spacing_radians)
 
-        # Find the average distance of each corresponding uniform point (between src and dst).
-        min_num_points = min(len(src_trench_points), len(dst_trench_points))
+        # Find the average distance of each uniform point in the dst trench (from the src trench).
         src_to_dst_distance = 0.0
-        for index in range(min_num_points):  # Common points
-            src_to_dst_distance += pygplates.GeometryOnSphere.distance(src_trench_points[index], dst_trench_points[index])
-        if len(src_trench_points) > len(dst_trench_points):
-            # Points only in src trench are tested against last point in dst trench.
-            for index in range(min_num_points, len(src_trench_points)):
-                src_to_dst_distance += pygplates.GeometryOnSphere.distance(src_trench_points[index], dst_trench_points[-1])
-            src_to_dst_distance /= len(src_trench_points)
-        else:
-            # Points only in dst trench are tested against last point in src trench.
-            for index in range(min_num_points, len(dst_trench_points)):
-                src_to_dst_distance += pygplates.GeometryOnSphere.distance(src_trench_points[-1], dst_trench_points[index])
-            src_to_dst_distance /= len(dst_trench_points)
+        for index in range(len(dst_trench_points)):
+            src_to_dst_distance += pygplates.GeometryOnSphere.distance(src_trench_line, dst_trench_points[index])
+        src_to_dst_distance /= len(dst_trench_points)
         
         # Is the current src trench the closest so far (to the current dst trench).
         if src_to_dst_distance < closest_src_to_dst_distance:
