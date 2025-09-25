9c1fc0abb7bb3babf3935a2e8758c8f6370bd62c
John Cannon <john.cannon@sydney.edu.au>
2025-09-25 16:00:49 +1000

    Set version to release candidate 3.

--- pybacktrack/version.py
+++ pybacktrack/version.py
@@ -3,7 +3,7 @@ VERSION_MINOR = 5
 VERSION_PATCH = 0
 # Pre-release suffix should follow the PEP440 versioning scheme (https://www.python.org/dev/peps/pep-0440/).
 # Note: It should be empty for public releases.
-VERSION_PRERELEASE_SUFFIX = 'rc2'
+VERSION_PRERELEASE_SUFFIX = 'rc3'
 
 __version__ = '{}.{}.{}{}'.format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_PRERELEASE_SUFFIX)
 VERSION = __version__
